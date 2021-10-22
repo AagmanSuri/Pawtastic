6fc8d14a14e1c54f3cc34f9fddc5776e335840b3
Aagman Suri <aagmansuri96@gmail.com>
2021-10-22 15:40:22 +0530

    home page text added css changed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,22 @@
 import React from "react";
-// import "./Home.css";
-// import DogBanner from "../../assets/dogBanner.svg";
+import "./Home.css";
+import { Link } from "react-router-dom";
+//import DogBanner from "../../assets/dogBanner.svg";
 // import Header from "../../components/Header/Header";
 const Home = () => {
   return (
-    <div>
-      <div>
-        <h2>Home</h2>
+    <section className="right">
+      <div className="center">
+        <h1>we care for</h1>
+        <h1>your furry little </h1>
+        <h1>loved ones </h1>
+        <h1>while </h1>
+        <h1>you’re away</h1>
       </div>
-    </div>
+      <Link to="/Services">
+        <button className="btn">Sechule Visit</button>
+      </Link>
+    </section>
   );
 };
 
