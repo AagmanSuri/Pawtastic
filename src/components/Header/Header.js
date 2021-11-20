6a980240b8186abc55108222b95a26986547c12e
Aagman Suri <aagmansuri96@gmail.com>
2021-11-20 21:27:43 +0530

    updates on photo of home , logo added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./Header.css";
+import Logo from "../../assets/Logo.png";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <ul id="nav">
+      <div className="Logo">
         <Link className="NavLinks" to="/">
-          <li>Pawtastic</li>
+          <img src={Logo} alt="Logo"></img>
         </Link>
+        {""}
+        <h4>Pawtastic</h4>
+      </div>
+      <ul id="nav">
         <Link className="NavLinks" to="/About">
           <li>About</li>
         </Link>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,31 +1,44 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-//import DogBanner from "../../assets/dogBanner.svg";
+import DogBanner from "../../assets/dogBanner.svg";
+
 // import Header from "../../components/Header/Header";
 const Home = () => {
   return (
-    <section>
-      <div className="right center">
-        <h1>we care for</h1>
-        <h1>your furry little </h1>
-        <h1>loved ones </h1>
-        <h1>while </h1>
-        <h1>you’re away</h1>
-        <Link to="/Services">
-          <button className="btn">Sechule Visit</button>
-        </Link>
-      </div>
-      <div>
-        <h2>Expert care for your furry, feathery, or scaley friend</h2>
-        <p>
-          We know how stressful it is to leave your pets at home alone. We’re a
-          team of experienced animal caregivers, well connected to local
-          veterinarians. Trust to us to love them like our own, and to keep them
-          safe and happy till you’re home.
-        </p>
-      </div>
-    </section>
+    <>
+      <section className="back">
+        <img src={DogBanner} alt="Logo"></img>
+      </section>
+      <section className="front">
+        <div className="right center">
+          <h1>we care for</h1>
+          <h1>your furry little </h1>
+          <h1>loved ones </h1>
+          <h1>while </h1>
+          <h1>you’re away</h1>
+          <Link to="/Services">
+            <button className="btn">Sechule Visit</button>
+          </Link>
+        </div>
+      </section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <section>
+        <div>
+          <h2>Expert care for your furry, feathery, or scaley friend</h2>
+          <p>
+            We know how stressful it is to leave your pets at home alone. We’re
+            a team of experienced animal caregivers, well connected to local
+            veterinarians. Trust to us to love them like our own, and to keep
+            them safe and happy till you’re home.
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
