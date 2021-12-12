e6dc4fd6872ab5ea75dea7721a123f50e205303d
Aagman Suri <aagmansuri96@gmail.com>
2021-12-13 01:05:44 +0530

    register

--- src/pages/Signup/register.js
+++ src/pages/Signup/register.js
@@ -1,11 +1,116 @@
 import React from "react";
-
-const register = () => {
+import "./signup.css";
+import BackGroundCat from "../../assets/backGroungCat.svg";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/Logo.svg";
+import { Formik } from "formik";
+import { useHistory } from "react-router";
+const Register = () => {
+  const history = useHistory();
   return (
-    <div>
-      <h2>Hello</h2>
+    <div className="wrapper wrapper-sign">
+      <img className="back-pic" src={BackGroundCat} alt="back"></img>
+      <div style={{ textAlign: "center", marginTop: "5rem" }}>
+        <div
+          style={{ marginTop: "-2rem", marginLeft: "-15rem" }}
+          className="Logo"
+        >
+          <Link className="NavLinks" to="/">
+            <img style={{ marginLeft: "1rem" }} src={Logo} alt="Logo"></img>
+          </Link>{" "}
+          <Link className="NavLinks" to="/">
+            {"    "}
+            <h2 style={{ marginLeft: "1rem", marginTop: "0.65rem" }}>
+              Pawtastic
+            </h2>
+          </Link>
+        </div>
+        <h1 style={{ color: "white" }}>Why our service?</h1>
+        <p style={{ color: "white", fontSize: "20px", marginRight: "2rem" }}>
+          <br></br>
+          We’re animal lovers backed by insurance and experience<br></br>{" "}
+          Powered by tech, so you can book and pay from our app <br></br>{" "}
+          Updates and pics for every visit keep you in the loop
+        </p>
+      </div>
+      <div
+        className="box2"
+        style={{
+          backgroundColor: "#F7ECEB"
+        }}
+      >
+        <div
+          style={{
+            marginLeft: "2rem",
+            marginTop: "16rem"
+          }}
+        >
+          <h1>
+            Good news! We care for pets in Winter Park. Let’s create your
+            account.
+          </h1>
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = "Required";
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = "Invalid email address";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              // setTimeout(() => {
+              //   alert(JSON.stringify(values, null, 2));
+              //   setSubmitting(false);
+              // }, 400);
+              history.push("./registerHumanProfile");
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting
+              /* and other goodies */
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <label>Email : </label>
+                <input
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                {errors.email && touched.email && errors.email}
+                {"  "}
+                <label>Password : </label>
+                <input
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                {errors.password && touched.password && errors.password}
+                {"     "}
+                <button className="btn" type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default register;
+export default Register;
