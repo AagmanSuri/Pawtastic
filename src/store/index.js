cadeb093335b037bfcd07d93fcad098570815e41
Aagman Suri <aagmansuri96@gmail.com>
2021-12-24 18:39:30 +0530

    authSlice Created

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./reducers/authSlice";
 import userProfileSlice from "./reducers/userProfileSlice";
 
 export const store = configureStore({
   reducer: {
-    userProfile: userProfileSlice
+    userProfile: userProfileSlice,
+    auth: authSlice
   }
 });
